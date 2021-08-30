53480b4447db99b165ad283e34f9d0205c41807f
Vlad-coder-678 <vova.mezencev@rambler.ru>
2021-08-30 21:26:19 +0300

    feat: add search and hotKeys

--- scripts/index.js
+++ scripts/index.js
@@ -17,6 +17,12 @@ const videoPlayerData = [
     src: "./assets/videos/Plexus",
     duration: null,
   },
+  {
+    id: 3,
+    name: "River",
+    src: "./assets/videos/River",
+    duration: null,
+  },
 ];
 
 // определяем поддерживаемый формат видео браузером и предагаем использовать видео в данном формате
@@ -36,6 +42,7 @@ function getFormatExtension(v) {
 function setVideoDOM(data) {
   let box = document.getElementById("videoSelection");
   const len = data.length;
+  box.innerHTML = "";
   for (let i = 0; i < len; i++) {
     let el = document.createElement("a");
     el.className = "videoSelection unSelectedVideo videoItem";
@@ -45,8 +52,8 @@ function setVideoDOM(data) {
   }
 }
 
+// вешаем слушатели на кнопки управления видео
 function controlListener(video, data) {
-  // let id = event.target.getAttribute("id");
   document.getElementById("previous").onclick = () => {
     prevVideo(video, data);
   };
@@ -65,26 +72,41 @@ function controlListener(video, data) {
   document.getElementById("mute").onclick = () => {
     muteVideo(video);
   };
-
-  // document.getElementById("video_progress_bar").onclick = currentTimeInit(
-  //   Number(event.offsetX)
-  // );
-  // document.getElementById("volume").onclick = () => {
-  // volumeInit(Number(event.offsetX) / 100);
-  // };
-
-  // document.getElementById("input_searcher").oninput = () => {
-  //   valueSearcher = event.target.value;
-  //   if (valueSearcher.length >= 3 && valueSearcher.length <= 20) {
-  //     setVideoInDom(searching(valueSearcher));
-  //     durationInit();
-  //   } else {
-  //     audios = audiosArray;
-  //     setVideoInDom(audios);
-  //     durationInit();
-  //   }
-  // };
+  document.getElementById("volume_bar").addEventListener("input", (e) => {
+    volumeInit(Number(e.target.value) / 100, video);
+  });
+  document.getElementById("input_searcher").oninput = (e) => {
+    console.log(e.target.value);
+    valueSearcher = e.target.value;
+    const len = valueSearcher.length;
+    if (len >= 3 && len < 20) {
+      setVideoDOM(searching(valueSearcher, data));
+      videoList(video, searching(valueSearcher, data), 0);
+      pushUnpushButtons("video0", []); // выбор первого видео
+    } else {
+      setVideoDOM(data);
+      videoList(video, data, 0);
+      pushUnpushButtons("video0", []); // выбор первого видео
+    }
+  };
+  document.getElementById("fullScreen").onclick = () => {
+    fullscreenVideo();
+  };
 }
+
+// отработка полноэкранного режима
+const fullscreenVideo = () => {
+  const elem = document.getElementById("video");
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) {
+    /* Safari */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) {
+    /* IE11 */
+    elem.msRequestFullscreen();
+  }
+};
 
 //  проверка нажата ли кнопка или нет
 function isButtonPushed(id) {
@@ -117,6 +139,7 @@ function pushUnpushButtons(idPush, arrIdUnpush) {
   }
 }
 
+// для обработки нажатия на кнопку PREV
 function prevVideo(v, vs) {
   let currV = getId() - 1; // текущий id
   let arrayId = [];
@@ -137,6 +160,7 @@ function prevVideo(v, vs) {
   v.play();
 }
 
+// для обработки нажатия на кнопку PLAY
 const playVideo = (v) => {
   if (!isButtonPushed("play" || "big_play")) {
     pushUnpushButtons("play", ["pause"]);
@@ -149,6 +173,7 @@ const playVideo = (v) => {
   }
 };
 
+// для обработки нажатия на кнопку NEXT
 function nextVideo(v, vs) {
   let currV = getId() + 1; // текущий id
   let arrayId = [];
@@ -272,10 +297,123 @@ function errorHandler() {
   }
 }
 
+// находим прогресс бар
+const findProgressBar = (v) => {
+  let progress_bar = document.getElementById("video_progress_bar");
+  let duration;
+  let currentTime;
+  let position;
+  v.addEventListener("loadedmetadata", () => {
+    duration = v.duration;
+  });
+  v.addEventListener("timeupdate", () => {
+    currentTime = v.currentTime;
+    position = (currentTime * 100) / duration;
+    progress_bar.style.backgroundImage = `linear-gradient(
+      to right,
+      #0075ff 0%,
+      #0075ff ${position}%,
+      #fff ${position}%,
+      #fff 100%
+  )`;
+  });
+  progress_bar.addEventListener("input", (e) => {
+    v.currentTime = e.target.value * duration * 0.01;
+  });
+};
+
+// отрисовка громкости звука браузера
+function volumeInit(volume, video) {
+  let volume_bar = document.getElementById("volume_bar");
+  let currVolume = volume * 100;
+  video.volume = volume;
+  volume_bar.value = currVolume;
+  volume_bar.style.background = `linear-gradient(
+    to right,
+    #0075ff 0%,
+    #0075ff ${currVolume}%,
+    #fff ${currVolume}%,
+    #fff 100%
+)`;
+}
+
+// для строки поиска
+function searching(value, data) {
+  let videos = [];
+  let len = data.length;
+  for (let i = 0; i < len; i++) {
+    if (data[i].name.search(new RegExp(value, "i")) != -1) {
+      videos.push(data[i]);
+    }
+  }
+  return videos;
+}
+
+// вешаем слушатели на окно
+const eventWindow = () => {
+  window.addEventListener("keyup", (e) => {
+    switch (e.key) {
+      case "f":
+        return fullscreenVideo();
+      case "m":
+        return muteVideo(video);
+      case "p":
+        return playVideo(video);
+      case "Escape":
+        return muteVideo(video);
+      case " ":
+        return playVideo(video);
+      case "ArrowLeft":
+        return slowerVideo(video);
+      case "ArrowRight":
+        return fasterVideo(video);
+      case "ArrowUp":
+        return louderVideo(video);
+      case "ArrowDown":
+        return quitVideo(video);
+      case "l":
+        return nextCadr(video);
+      case "j":
+        return prevCadr(video);
+    }
+  });
+};
+
+// перематываем на 10 сек назад
+const nextCadr = (v) => {
+  v.currentTime -= 10;
+};
+
+// перематываем на 10 сек вперёд
+const prevCadr = (v) => {
+  v.currentTime += 10;
+};
+
+// увеличиваем громкость
+const louderVideo = (v) => {
+  volumeInit(v.volume + 0.1 > 1 ? 1 : v.volume + 0.1, v);
+};
+
+// уменьшаем громкость
+const quitVideo = (v) => {
+  volumeInit(v.volume - 0.1 <= 0.1 ? 0 : v.volume - 0.1, v);
+};
+
+// замедляем видео
+const slowerVideo = (v) => {
+  v.playbackRate /= 1.1;
+};
+
+// ускоряем видео
+const fasterVideo = (v) => {
+  v.playbackRate *= 1.1;
+};
+
 window.onload = () => {
   const videos = videoPlayerData;
   let id = 0;
   let video = document.getElementById("video");
+  let initVolume = 0.2;
   video.name = id;
   video.src = videos[id].src + getFormatExtension(video);
   video.load();
@@ -284,15 +422,10 @@ window.onload = () => {
   controlListener(video, videos); // вешаем обработчики на кнопки управления
   videoList(video, videos, id); // вешаем обработчики на кнопки с видео
   eventListener(video, videos, id); // вешаем слушатели на события видео
+  eventWindow(); // вешаем слушатели на события окна
   setMetaData(video, id, videos); // при загрузке метаданных выводим их в дом
+  findProgressBar(video); // работа с прогресс баром
+  volumeInit(initVolume, video); // отрисовка громкости звука браузера
 
-  pushUnpushButtons("video0", []);
-
-  // находим прогрес бар и считываем value
-  // let progress = document.getElementByld("video_progress_bar");
-  // progress.addEventListener("input", function () {
-  //   const value = this.value;
-  //   this.style.border = `1px solid red`;
-  //   this.style.background = `linear-gradient(to right, #82CFD0 0%, #82CFD0 ${value}%, #fff ${value}%, white 100%)`;
-  // });
+  pushUnpushButtons("video0", []); // выбор первого видео
 };
